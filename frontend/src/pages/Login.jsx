e9e16114e9148290dbd187533cd601a205a3d478
santy <santy.acevedo2005@gmail.com>
2025-07-02 20:16:37 -0500

    feat: :zap: se crea carpeta para las paginas y conexion

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Login() {
+  return (
+    <div style={{ padding: '2rem', textAlign: 'center' }}>
+      <h2>Login Page</h2>
+      <p>formulario de pintabotones</p>
+    </div>
+  )
+}
+
+export default Login
